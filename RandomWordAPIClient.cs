6020ed8feea9fd969943c22b55935803dce56289
APIVerve Team <hello@apiverve.com>
2024-08-20 11:05:39 -0500

    Bug Fixes

--- RandomWordAPIClient.cs
+++ RandomWordAPIClient.cs
@@ -57,7 +57,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="callback"></param>
         /// <param name="options"></param>
-        public void ExecuteAsync(ExecuteAsyncCallback callback, randomwordsQueryOptions options = null)
+        public void ExecuteAsync(ExecuteAsyncCallback callback, RandomWordQueryOptions options = null)
         {
             ThreadPool.QueueUserWorkItem(state =>
             {
@@ -71,7 +71,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="options"></param>
         /// <returns></returns>
-        public ResponseObj Execute(randomwordsQueryOptions options = null)
+        public ResponseObj Execute(RandomWordQueryOptions options = null)
         {
             try
             {
@@ -89,6 +89,7 @@ namespace APIVerve
 
                 var request = WebRequest.Create(url);
                 request.Headers["x-api-key"] = _apiKey;
+                request.Headers["auth-mode"] = "nuget-package";
                 request.Method = _method;
 
                 if (_method == "POST")
@@ -149,7 +150,7 @@ namespace APIVerve
             }
         }
 
-        private string constructURL(randomwordsQueryOptions options)
+        private string constructURL(RandomWordQueryOptions options)
         {
             string url = _apiEndpoint;
 
